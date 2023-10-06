89e4d4b5892943774180104e71754b45bd24dc20
VITIPON <alexvitip@gmail.com>
2023-10-06 12:22:27 +0100

    Clean pages and header footer done

--- wp-content/themes/oceanwp-child-theme-master/functions.php
+++ wp-content/themes/oceanwp-child-theme-master/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * OceanWP Child Theme Functions
  *
@@ -19,15 +20,40 @@
  *
  * @link http://codex.wordpress.org/Child_Themes
  */
-function oceanwp_child_enqueue_parent_style() {
-
+function oceanwp_child_enqueue_parent_style()
+{
 	// Dynamically get version number of the parent stylesheet (lets browsers re-cache your stylesheet when you update the theme).
-	$theme   = wp_get_theme( 'OceanWP' );
-	$version = $theme->get( 'Version' );
+	$theme   = wp_get_theme('OceanWP');
+	$version = $theme->get('Version');
 
 	// Load the stylesheet.
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'oceanwp-style' ), $version );
-	
+	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('oceanwp-style'), $version);
 }
 
-add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
+function montheme_supports()
+{
+	add_theme_support('menus');
+	register_nav_menus(array(
+		'menu-1' => 'primary',
+	));
+}
+
+// Adds nav-item class to the li items in the menu
+function montheme_menu_class($classes)
+{
+	$classes[] = 'nav-item';
+	return $classes;
+}
+
+// loads the parent styling
+add_action('wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style');
+// Runs montheme_menu_class to add the nav-item
+add_filter('nav_menu_css_class', 'montheme_menu_class');
+
+// Load child styling
+function my_theme_enqueue_styles() {
+	wp_register_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style' );
+  
+  }
+  add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
--- wp-content/themes/oceanwp-child-theme-master/header.php
+++ wp-content/themes/oceanwp-child-theme-master/header.php
@@ -1,40 +1,63 @@
-<?php
-/**
- * The Header for our theme.
- *
- * @package OceanWP WordPress theme
- */
+	<?php
+	/**
+	 * The Header for our theme.
+	 *
+	 * @package OceanWP WordPress theme
+	 */
 
-?>
-<!DOCTYPE html>
-<html class="<?php echo esc_attr( oceanwp_html_classes() ); ?>" <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+	?>
+	<!DOCTYPE html>
+	<html class="<?php echo esc_attr( oceanwp_html_classes() ); ?>" <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
-</head>
+		<?php wp_head(); ?>
+	</head>
 
-<body <?php body_class(); ?> <?php oceanwp_schema_markup( 'html' ); ?>>
+	<body <?php body_class(); ?> <?php oceanwp_schema_markup( 'html' ); ?>>
 
-	<?php wp_body_open(); ?>
+		<?php wp_body_open(); ?>
 
-	<?php do_action( 'ocean_before_outer_wrap' ); ?>
+		<?php do_action( 'ocean_before_outer_wrap' ); ?>
 
-	<div id="outer-wrap" class="site clr">
+		<div id="outer-wrap" class="site clr">
 
-		<a class="skip-link screen-reader-text" href="#main"><?php echo esc_html( oceanwp_theme_strings( 'owp-string-header-skip-link', false ) ); ?></a>
+			<a class="skip-link screen-reader-text" href="#main"><?php echo esc_html( oceanwp_theme_strings( 'owp-string-header-skip-link', false ) ); ?></a>
 
-		<?php do_action( 'ocean_before_wrap' ); ?>
+			<?php do_action( 'ocean_before_wrap' ); ?>
 
-		<div id="wrap" class="clr">
+			<div id="wrap" class="clr">
 
-			<?php do_action( 'ocean_top_bar' ); ?>
 
-			<?php do_action( 'ocean_header' ); ?>
+				<header>
 
-			<?php do_action( 'ocean_before_main' ); ?>
 
-			<main id="main" class="site-main clr"<?php oceanwp_schema_markup( 'main' ); ?> role="main">
+	<div class="site-branding">
 
-				<?php do_action( 'ocean_page_header' ); ?>
+
+		<a href="<?php echo home_url(); ?>">
+		<img src="<?php echo get_template_directory_uri(); ?>/logo.png" alt="Site Logo">
+		</a>
+
+
+		<div class="menu">
+		<?php 
+			wp_nav_menu(array(
+			'menu' => 'menu-1',
+			'theme_location' => 'primary',
+			'container' => false,
+			'menu_class' => 'navbar-nav mr-auto',
+			));  
+		?>
+		</div>
+
+	</div>
+
+	</header>
+
+				<?php do_action( 'ocean_before_main' ); ?>
+
+				<main id="main" class="site-main clr"<?php oceanwp_schema_markup( 'main' ); ?> role="main">
+
+					<?php do_action( 'ocean_page_header' ); ?>
